2300135f618a2c9594867dc9fd0bcb24c59f244e
yedajiang44 <602830483@qq.com>
2021-03-03 00:49:01 +0800

    添加docker

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nginx:alpine
+COPY ./dist/emulator /usr/share/nginx/html
---
+++ build.sh
@@ -0,0 +1,2 @@
+ng build --prod
+docker build --pull --rm --no-cache -f "Dockerfile" -t yedajiang44/jt808terminalemulator-ui "."
